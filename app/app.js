7f22e9d66b9ad5f973597e9dbe0aad60b2e846ef
Jake Litwicki <jake.litwicki@gmail.com>
2019-10-22 15:16:27 -0700

    simplify using @dabit3 skeleton for amplify auth

--- app/app.js
+++ app/app.js
@@ -33,6 +33,8 @@ import configureStore from './configureStore';
 // Import i18n messages
 import { translationMessages } from './i18n';
 
+import * as serviceWorker from './serviceWorker';
+
 // Create redux store with history
 const initialState = {};
 const store = configureStore(initialState, history);
--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -8,20 +8,14 @@
  */
 
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-
-import HomePage from 'containers/HomePage/Loadable';
-import NotFoundPage from 'containers/NotFoundPage/Loadable';
 
 import GlobalStyle from '../../global-styles';
+import Router from '../../components/Router';
 
 export default function App() {
   return (
     <div>
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route component={NotFoundPage} />
-      </Switch>
+      <Router />
       <GlobalStyle />
     </div>
   );
