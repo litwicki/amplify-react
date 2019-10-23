3329ffd93b153a70b77cba2644b43270aec9d92e
Jake Litwicki <jake.litwicki@gmail.com>
2019-10-22 17:56:50 -0700

    update router

--- app/components/Router/index.js
+++ app/components/Router/index.js
@@ -4,31 +4,87 @@
  *
  */
 
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+
+// import PropTypes from 'prop-types';
+// import styled from 'styled-components';
+
+// import { FormattedMessage } from 'react-intl';
+// import messages from './messages';
+
+import React from 'react'
+import {
+  withRouter,
+  Switch,
+  Route,
+  Redirect,
+  BrowserRouter as Router
+} from 'react-router-dom'
+import { Auth } from 'aws-amplify'
 
 import HomePage from 'containers/HomePage/Loadable';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
 import AuthPage from 'containers/AuthPage';
-// import PropTypes from 'prop-types';
-// import styled from 'styled-components';
+import ProfilePage from 'containers/ProfilePage';
 
-import { FormattedMessage } from 'react-intl';
-import messages from './messages';
-
-function Router() {
-  return (
-    <div>
-      {/* <FormattedMessage {...messages.header} /> */}
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/auth" component={AuthPage} />
-        <Route component={NotFoundPage} />
-      </Switch>
-    </div>
-  );
+class PrivateRoute extends React.Component {
+  state = {
+    loaded: false,
+    isAuthenticated: false
+  }
+  componentDidMount() {
+    this.authenticate()
+    this.unlisten = this.props.history.listen(() => {
+      Auth.currentAuthenticatedUser()
+        .then(user => console.log('user: ', user))
+        .catch(() => {
+          if (this.state.isAuthenticated) this.setState({ isAuthenticated: false })
+        })
+    });
+  }
+  componentWillUnmount() {
+    this.unlisten()
+  }
+  authenticate() {
+    Auth.currentAuthenticatedUser()
+      .then(() => {
+        this.setState({ loaded: true, isAuthenticated: true })
+      })
+      .catch(() => this.props.history.push('/auth'))
+  }
+  render() {
+    const { component: Component, ...rest } = this.props
+    const { loaded , isAuthenticated} = this.state
+    if (!loaded) return null
+    return (
+      <Route
+        {...rest}
+        render={props => {
+          return isAuthenticated ? (
+            <Component {...props} />
+          ) : (
+            <Redirect
+              to={{
+                pathname: "/auth",
+              }}
+            />
+          )
+        }}
+      />
+    )
+  }
 }
 
-Router.propTypes = {};
+PrivateRoute = withRouter(PrivateRoute)
 
-export default Router;
+const Routes = () => (
+  <Router>
+    <Switch>
+      <Route path='/auth' component={AuthPage} />
+      <PrivateRoute path='/profile' component={ProfilePage} />
+      <Route exact path="/" component={HomePage} />
+      <Route component={NotFoundPage} />
+    </Switch>
+  </Router>
+)
+
+export default Routes
